416b380d9b34e67807036559b5a67ba7437662e3
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2024-01-10 21:33:29 +0300

    консольное приложение №1 исправление ошибки

--- src/main/java/Formatter.java
+++ src/main/java/Formatter.java
@@ -1,6 +1,8 @@
 public class Formatter {
     public static String rubl(double sum) {
         String b = "";
+        if (sum >= 11 && sum <= 19)
+            b="рублей";
         if (sum % 10 == 1)
             b = "рубль";
         if (sum % 10 > 1 && sum % 10 < 5)
